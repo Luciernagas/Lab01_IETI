06c84780989791143c68ca7b9fac8583bcf028cc
Luciernagas <104604359+Luciernagas@users.noreply.github.com>
2024-01-31 17:52:01 -0500

    Initial commit

---
+++ src/test/java/com/example/lab01_ieti/Lab01IetiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab01_ieti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab01IetiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
